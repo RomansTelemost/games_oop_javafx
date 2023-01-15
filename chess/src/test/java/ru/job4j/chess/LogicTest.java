5d94a37674ae72b78f0ae1def36f886e9ff5add9
Roman Koshelev <termopsis@mail.ru>
2023-01-15 13:51:13 +0300

    2. Каркас шахматной доски [#792 #381164] - fix

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -1,14 +1,15 @@
 package ru.job4j.chess;
 
+import javafx.scene.Group;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import ru.job4j.chess.firuges.Cell;
-import ru.job4j.chess.firuges.black.BishopBlack;
+import ru.job4j.chess.firuges.black.*;
+import ru.job4j.chess.firuges.white.*;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@Disabled("Тесты отключены. Удалить аннотацию после реализации всех методов по заданию.")
 public class LogicTest {
 
     @Test
@@ -19,5 +20,26 @@ public class LogicTest {
             logic.move(Cell.C1, Cell.H6);
         });
         assertThat(exception.getMessage()).isEqualTo("Figure not found on the board.");
+    }
+
+    @Test
+    public void whenMoveBlackBishopThenFigureOccupiedCellException()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.C8));
+        logic.add(new PawnBlack(Cell.B7));
+        OccupiedCellException exception = assertThrows(OccupiedCellException.class, () -> {
+            logic.move(Cell.C8, Cell.B7);
+        });
+    }
+
+    @Test
+    public void whenMoveBlackBishopThenFigureImpossibleMoveException()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.C8));
+        ImpossibleMoveException exception = assertThrows(ImpossibleMoveException.class, () -> {
+            logic.move(Cell.C8, Cell.B8);
+        });
     }
 }
